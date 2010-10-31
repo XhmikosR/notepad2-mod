94b67abb2a378a34cc56d558369287874d3f70a4
XhmikosR <xhmikosr@users.sourceforge.net>
2010-10-31 18:20:27 +0000

    update setup project
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@8 28bd50df-7adb-d945-0439-6e466c6a13cc

--- setup/version.h
+++ setup/version.h
@@ -1,6 +1,6 @@
-#define SETUP_VERSION_FULL  4,0,22,5
-#define SETUP_VERSION_STR   "4.0.22-beta5 (modified by Kai Liu; rev. 5)"
-#define SETUP_VERSION_LABEL "4.0.22-beta5 (modified; rev. 5)"
+#define SETUP_VERSION_FULL  4,1,24,6
+#define SETUP_VERSION_STR   "4.1.24 (modified by Kai Liu and XhmikosR; rev. 6)"
+#define SETUP_VERSION_LABEL "4.1.24 (modified; rev. 6)"
 #define SETUP_TITLE_STR     "Notepad2 Setup"
 #define SETUP_AUTHOR_STR    ""
 #define SETUP_COPYRIGHT_STR ""
