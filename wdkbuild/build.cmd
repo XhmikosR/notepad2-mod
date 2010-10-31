b090cd791aaaa3d87318ebf7a442cccfec9cc3d2
XhmikosR <xhmikosr@users.sourceforge.net>
2010-10-31 17:35:32 +0000

    applied the needed change in KeyWords.cxx for notepad2 to build, updated the ico, the toolbar and the manifest with the ones Kai Liu used
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@4 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/build.cmd
+++ wdkbuild/build.cmd
@@ -1,19 +1,21 @@
 @echo off
-
-rem set WDKBASEDIR=C:\WinDDK\7600.16385.1
-set WDKBASEDIR=D:\Programme\Microsoft wdk7
+setlocal
+set WDKBASEDIR=C:\progs\WinDDK\7600.16385.1
 
 set INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk
 set LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386
 set LIBPATH=
 set VSCOMNTOOLS=
 
-md "../Release"
-del "../Release/*.obj"
-del "../Release/*.exe"
-del "../Release/*.pdb"
-del "../Release/*.idb"
+md "../Release" >NUL 2>&1
+del "../Release/*.obj" >NUL 2>&1
+del "../Release/*.exe" >NUL 2>&1
+del "../Release/*.pdb" >NUL 2>&1
+del "../Release/*.idb" >NUL 2>&1
 
 "%WDKBASEDIR%\bin\x86\x86\cl.exe" @cl.txt
 "%WDKBASEDIR%\bin\x86\rc.exe" /fo"../Release/Notepad2.res" "..\src\Notepad2.rc"
 "%WDKBASEDIR%\bin\x86\x86\link.exe" @link.txt
+
+endlocal
+pause
