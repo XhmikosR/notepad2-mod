66638c92337ac58fc710dfa167ac26ed8ba7a4ca
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-01 10:39:14 +0000

    merge from trunk r31-32
    git-svn-id: https://notepad2-mod.googlecode.com/svn/branches/notepad2_4.1.24@34 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/build.cmd
+++ wdkbuild/build.cmd
@@ -5,14 +5,14 @@ set WDKBASEDIR=C:\progs\WinDDK\7600.16385.1
 set INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk
 set LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386
 
-md "../Release" >NUL 2>&1
-del "../Release/*.obj" >NUL 2>&1
+md "../Release/obj" >NUL 2>&1
 del "../Release/*.exe" >NUL 2>&1
-del "../Release/*.pdb" >NUL 2>&1
-del "../Release/*.idb" >NUL 2>&1
+del "../Release/obj/*.obj" >NUL 2>&1
+del "../Release/obj/*.pdb" >NUL 2>&1
+del "../Release/obj/*.idb" >NUL 2>&1
 
 "%WDKBASEDIR%\bin\x86\x86\cl.exe" @cl.txt
-"%WDKBASEDIR%\bin\x86\rc.exe" /fo"../Release/Notepad2.res" "..\src\Notepad2.rc"
+"%WDKBASEDIR%\bin\x86\rc.exe" /fo"../Release/obj/Notepad2.res" "..\src\Notepad2.rc"
 "%WDKBASEDIR%\bin\x86\x86\link.exe" @link.txt
 
 endlocal
