cc7bd5d46d35ad0673120f4a98388050ae90d8c1
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-02 15:53:47 +0000

    notepad2_4.1.24 branch: use the same command line for the wdk builds as the vanilla notepad2 does
    git-svn-id: https://notepad2-mod.googlecode.com/svn/branches/notepad2_4.1.24@54 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/build.cmd
+++ wdkbuild/build.cmd
@@ -1,19 +1,22 @@
 @echo off
 setlocal
-set WDKBASEDIR=C:\progs\WinDDK\7600.16385.1
+set WDKBASEDIR=C:\WinDDK\7600.16385.1
 
 set INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk
 set LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386
 
-md "../Release/obj" >NUL 2>&1
-del "../Release/*.exe" >NUL 2>&1
-del "../Release/obj/*.obj" >NUL 2>&1
-del "../Release/obj/*.pdb" >NUL 2>&1
-del "../Release/obj/*.idb" >NUL 2>&1
+md "..\Release\obj" >NUL 2>&1
+del "..\Release\*.exe" >NUL 2>&1
+del "..\Release\obj\*.obj" >NUL 2>&1
+del "..\Release\obj\*.pdb" >NUL 2>&1
+del "..\Release\obj\*.idb" >NUL 2>&1
 
 "%WDKBASEDIR%\bin\x86\x86\cl.exe" @cl.txt
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 "%WDKBASEDIR%\bin\x86\rc.exe" /fo"../Release/obj/Notepad2.res" "..\src\Notepad2.rc"
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 "%WDKBASEDIR%\bin\x86\x86\link.exe" @link.txt
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 
 endlocal
-pause
+rem pause
--- wdkbuild/build_x64.cmd
+++ wdkbuild/build_x64.cmd
@@ -1,19 +1,22 @@
 @echo off
 setlocal
-set WDKBASEDIR=C:\progs\WinDDK\7600.16385.1
+set WDKBASEDIR=C:\WinDDK\7600.16385.1
 
 set INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk
 set LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64
 
-md "../Release_x64/obj" >NUL 2>&1
-del "../Release_x64/*.exe" >NUL 2>&1
-del "../Release_x64/obj/*.obj" >NUL 2>&1
-del "../Release_x64/obj/*.pdb" >NUL 2>&1
-del "../Release_x64/obj/*.idb" >NUL 2>&1
+md "..\Release_x64\obj" >NUL 2>&1
+del "..\Release_x64\*.exe" >NUL 2>&1
+del "..\Release_x64\obj\*.obj" >NUL 2>&1
+del "..\Release_x64\obj\*.pdb" >NUL 2>&1
+del "..\Release_x64\obj\*.idb" >NUL 2>&1
 
 "%WDKBASEDIR%\bin\x86\amd64\cl.exe" @cl_x64.txt
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 "%WDKBASEDIR%\bin\x86\rc.exe" /fo"../Release_x64/obj/Notepad2.res" "..\src\Notepad2.rc"
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 "%WDKBASEDIR%\bin\x86\amd64\link.exe" @link_x64.txt
+IF %ERRORLEVEL% NEQ 0 ECHO:Compilation failed!&&PAUSE&&EXIT
 
 endlocal
-pause
+rem pause
