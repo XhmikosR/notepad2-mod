9e3b3f7249d9e73d268842db0a6daced112177b8
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-03 12:28:38 +0000

    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@70 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/make_packages.bat
+++ wdkbuild/make_packages.bat
@@ -30,14 +30,14 @@ ECHO.
 MD "temp_zip" >NUL 2>&1
 COPY "..\License.txt" "temp_zip\" /Y /V
 COPY "..\%1\Notepad2.exe" "temp_zip\" /Y /V
-COPY "..\distrib\Notepad2.ini" "temp_zip\" /Y /V
-COPY "..\distrib\notepad2.redir.ini" "temp_zip\" /Y /V
-COPY "..\distrib\setup_old\res\cabinet\Notepad2.inf" "temp_zip\" /Y /V
+COPY "..\distrib\res\cabinet\Notepad2.ini" "temp_zip\" /Y /V
+COPY "..\distrib\res\cabinet\notepad2.redir.ini" "temp_zip\" /Y /V
+COPY "..\distrib\res\cabinet\Notepad2.inf" "temp_zip\" /Y /V
 COPY "..\Notepad2.txt" "temp_zip\" /Y /V
 COPY "..\ReadMe-mod.txt" "temp_zip\Readme.txt" /Y /V
 
 PUSHD "temp_zip"
-START "" /B /WAIT "..\..\distrib\setup_old\tools\7za.exe" a -tzip -mx=9^
+START "" /B /WAIT "..\..\distrib\tools\7za.exe" a -tzip -mx=9^
  "Notepad2-mod.%NOTEPAD_VERSION%_r%SVNREV%_%2.zip" "License.txt" "Notepad2.exe"^
  "Notepad2.ini" "notepad2.redir.ini" "Notepad2.inf" "Notepad2.txt" "ReadMe.txt" >NUL
 IF %ERRORLEVEL% NEQ 0 GOTO :ErrorDetected
