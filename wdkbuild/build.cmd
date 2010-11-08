1ba77e9c3fcca3a157eb839e9344485def9edf5f
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-08 10:49:12 +0000

    add some checks in the build.cmd
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@126 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/build.cmd
+++ wdkbuild/build.cmd
@@ -6,14 +6,17 @@ rem Set the WDDK and SDK directories
 set WDKBASEDIR=C:\WinDDK\7600.16385.1
 set SDKDIR=C:\Program Files\Microsoft SDKs\Windows\v7.1
 
+IF NOT EXIST "%WDKBASEDIR%" ECHO:[ERROR] Specify your WDK dir&&PAUSE&&EXIT
+IF NOT EXIST "%SDKDIR%" ECHO:[ERROR] Specify your SDK dir&&PAUSE&&EXIT
+
 pushd ..
-call update_version.bat
+call "update_version.bat"
 popd
 
 rem x86
-set INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk
-set LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386
-set PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\x86;%PATH%
+set "INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk"
+set "LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386"
+set "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\x86;%PATH%"
 set OUTDIR=..\Release
 set OBJDIR=%OUTDIR%\obj
 
@@ -21,8 +24,8 @@ TITLE Building Notepad2 x86...
 call "build_base.bat" x86
 
 rem x64
-set LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64
-set PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\amd64;%PATH%
+set "LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64"
+set "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\amd64;%PATH%"
 set OUTDIR=..\Release_x64
 set OBJDIR=%OUTDIR%\obj
 
@@ -32,4 +35,4 @@ call "build_base.bat" x64
 :end
 TITLE Finished!
 endlocal
-goto :eof
+exit /b
