85099877836a73318af3be2222a936e7974aa3ac
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-09 10:44:01 +0000

    merge r140 from trunk
    git-svn-id: https://notepad2-mod.googlecode.com/svn/branches/notepad2@141 28bd50df-7adb-d945-0439-6e466c6a13cc

--- wdkbuild/build.cmd
+++ wdkbuild/build.cmd
@@ -1,48 +1,46 @@
-@echo off
-setlocal
-cd /d %~dp0
+@ECHO OFF
+SETLOCAL
+CD /D %~dp0
 
 rem Set the WDK and SDK directories
-set "WDKBASEDIR=C:\WinDDK\7600.16385.1"
-set "SDKDIR=%PROGRAMFILES%\Microsoft SDKs\Windows\v7.1"
+SET "WDKBASEDIR=C:\WinDDK\7600.16385.1"
+SET "SDKDIR=%PROGRAMFILES%\Microsoft SDKs\Windows\v7.1"
 
 rem Check the building environment
-IF NOT EXIST "%WDKBASEDIR%" (
-  ECHO. && ECHO:______________________________
-  ECHO:[ERROR] Specify your WDK directory
-  ECHO:______________________________ && ECHO.
-  PAUSE
-  EXIT
-)
-
-IF NOT EXIST "%SDKDIR%" (
-  ECHO. && ECHO:______________________________
-  ECHO:[ERROR] Specify your SDK directory
-  ECHO:______________________________ && ECHO.
-  PAUSE
-  EXIT
-)
+IF NOT EXIST "%WDKBASEDIR%" CALL :SUBMSG "ERROR" "Specify your WDK directory!"
+IF NOT EXIST "%SDKDIR%" CALL :SUBMSG "ERROR" "Specify your SDK directory!"
 
 rem x86
-set "INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk"
-set "LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386"
-set "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\x86;%PATH%"
-set OUTDIR=..\Release
-set OBJDIR=%OUTDIR%\obj
+SET "INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk"
+SET "LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386"
+SET "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\x86;%PATH%"
+SET OUTDIR=..\Release
+SET OBJDIR=%OUTDIR%\obj
 
 TITLE Building Notepad2 x86...
-call "build_base.bat" x86
+CALL "build_base.bat" x86
 
 rem x64
-set "LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64"
-set "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\amd64;%PATH%"
-set OUTDIR=..\Release_x64
-set OBJDIR=%OUTDIR%\obj
+SET "LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64"
+SET "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\amd64;%PATH%"
+SET OUTDIR=..\Release_x64
+SET OBJDIR=%OUTDIR%\obj
 
 TITLE Building Notepad2 x64...
-call "build_base.bat" x64
+CALL "build_base.bat" x64
 
 :end
 TITLE Finished!
-endlocal
-exit /b
+ENDLOCAL
+EXIT /B
+
+:SUBMSG
+ECHO.&&ECHO:______________________________
+ECHO:[%~1] %~2
+ECHO:______________________________&&ECHO.
+IF /I "%~1"=="ERROR" (
+  PAUSE
+  EXIT
+) ELSE (
+  EXIT /B
+)
