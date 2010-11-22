a67f1055dcaea5d945138a01cbfc227b0ca5ff9d
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-22 21:23:45 +0000

    improve Lua highlighting
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@260 28bd50df-7adb-d945-0439-6e466c6a13cc

--- src/Styles.c
+++ src/Styles.c
@@ -1118,7 +1118,26 @@ EDITLEXER lexRUBY = { SCLEX_RUBY, 63304, L"Ruby", L"rb; ruby", L"", &KeyWords_RU
 KEYWORDLIST KeyWords_LUA = {
 "and break do else elseif end false for function if in local nil not or repeat return then "
 "true until while",
-"", "", "", "", "", "", "", "" };
+"_VERSION assert collectgarbage dofile error gcinfo loadfile loadstring print rawget rawset "
+"require tonumber tostring type unpack _ALERT _ERRORMESSAGE _INPUT _PROMPT _OUTPUT _STDERR "
+"_STDIN _STDOUT call dostring foreach foreachi getn globals newtype sort tinsert tremove "
+"_G getfenv getmetatable ipairs loadlib next pairs pcall rawequal setfenv setmetatable xpcall "
+"string table math coroutine io os debug load module select",												// Basic Functions
+"abs acos asin atan atan2 ceil cos deg exp floor format frexp gsub ldexp log log10 max min "
+"mod rad random randomseed sin sqrt strbyte strchar strfind strlen strlower strrep strsub strupper tan "
+"string.byte string.char string.dump string.find string.len string.lower string.rep string.sub string.upper "
+"string.format string.gfind string.gsub table.concat table.foreach table.foreachi table.getn table.sort "
+"table.insert table.remove table.setn math.abs math.acos math.asin math.atan math.atan2 math.ceil math.cos "
+"math.deg math.exp math.floor math.frexp math.ldexp math.log math.log10 math.max math.min math.mod "
+"math.pi math.pow math.rad math.random math.randomseed math.sin math.sqrt math.tan string.gmatch "
+"string.match string.reverse table.maxn math.cosh math.fmod math.modf math.sinh math.tanh math.huge",		// String Manipulation, Table Manipulation, Mathematical Functions
+"openfile closefile readfrom writeto appendto remove rename flush seek tmpfile tmpname read "
+"write clock date difftime execute exit getenv setlocale time coroutine.create coroutine.resume "
+"coroutine.status coroutine.wrap coroutine.yield io.close io.flush io.input io.lines io.open io.output "
+"io.read io.tmpfile io.type io.write io.stdin io.stdout io.stderr os.clock os.date os.difftime "
+"os.execute os.exit os.getenv os.remove os.rename os.setlocale os.time os.tmpname coroutine.running "
+"package.cpath package.loaded package.loadlib package.path package.preload package.seeall io.popen",		// Input and Output Facilities & System Facilities Coroutine Manipulation, Input and Output Facilities, System Facilities (coroutine & io & os)
+"", "", "", "", "" };
 
 
 EDITLEXER lexLUA = { SCLEX_LUA, 63298, L"Lua Script", L"lua", L"", &KeyWords_LUA, {
@@ -1127,11 +1146,14 @@ EDITLEXER lexLUA = { SCLEX_LUA, 63298, L"Lua Script", L"lua", L"", &KeyWords_LUA
                     { MULTI_STYLE(SCE_LUA_COMMENT,SCE_LUA_COMMENTLINE,SCE_LUA_COMMENTDOC,0), 63127, L"Comment", L"fore:#007F00", L"" },
                     { SCE_LUA_NUMBER, 63130, L"Number", L"fore:#008080", L"" },
                     { SCE_LUA_WORD, 63128, L"Keyword", L"fore:#00007F", L"" },
-                    { MULTI_STYLE(SCE_LUA_STRING,SCE_LUA_STRINGEOL,0,0), 63131, L"String", L"fore:#FF0000", L"" },
-                    { SCE_LUA_LITERALSTRING, 63302, L"Literal String", L"fore:#FF0000", L"" },
-                    { SCE_LUA_CHARACTER, 63301, L"Character", L"fore:#FF0000", L"" },
+                    { SCE_LUA_WORD2, 63321, L"Basic Functions", L"fore:#00007F; back:#E8FFE8", L"" },
+                    { SCE_LUA_WORD3, 63322, L"String, Table & Math Functions", L"fore:#00007F; back:#EAEAFF", L"" },
+                    { SCE_LUA_WORD4, 63323, L"Input, Output & System Facilities", L"fore:#00007F; back:#FFECFF", L"" },
+                    { MULTI_STYLE(SCE_LUA_STRING,SCE_LUA_STRINGEOL,0,0), 63131, L"String", L"fore:#B000B0", L"" },
+                    { SCE_LUA_LITERALSTRING, 63302, L"Literal String", L"fore:#B000B0", L"" },
+                    { SCE_LUA_CHARACTER, 63301, L"Character", L"fore:#B000B0", L"" },
                     { SCE_LUA_PREPROCESSOR, 63133, L"Preprocessor", L"fore:#FF8000", L"" },
-                    { SCE_LUA_OPERATOR, 63132, L"Operator", L"fore:#B000B0", L"" },
+                    { SCE_LUA_OPERATOR, 63132, L"Operator", L"", L"" },
                     { SCE_LUA_IDENTIFIER, 63129, L"Identifier", L"", L"" },
                     { -1, 00000, L"", L"", L"" } } };
 
