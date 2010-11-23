b0eb53c9b3cbe0baf11e851a1d1c8e3661044489
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-23 14:01:39 +0000

    update installer's manifest to include the version
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@267 28bd50df-7adb-d945-0439-6e466c6a13cc

--- update_version.bat
+++ update_version.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 SubWCRev .\ src\Version_in.h src\Version_rev.h
 SubWCRev .\ res\Notepad2.exe.manifest.conf res\Notepad2.exe.manifest
+SubWCRev .\ distrib\res\full\setup.manifest.conf distrib\res\full\setup.manifest
+SubWCRev .\ distrib\res\lite\setup.manifest.conf distrib\res\lite\setup.manifest
 IF %ERRORLEVEL% NEQ 0 GOTO :NoSubWCRev
 GOTO :EOF
 
@@ -8,3 +10,5 @@ GOTO :EOF
 ECHO:NoSubWCRev, will use VERSION_REV=0
 ECHO:#define VERSION_REV 0 >src\Version_rev.h
 COPY /Y res\Notepad2.exe.manifest.template res\Notepad2.exe.manifest
+COPY /Y distrib\res\full\setup.manifest.template distrib\res\full\setup.manifest
+COPY /Y distrib\res\lite\setup.manifest.template distrib\res\lite\setup.manifest
