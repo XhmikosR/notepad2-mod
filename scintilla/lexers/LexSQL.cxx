f60a72dfd4f35d973c82d378cc436c4a5ef660a6
XhmikosR <xhmikosr@users.sourceforge.net>
2011-01-31 12:47:16 +0000

    update Scintilla
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@393 28bd50df-7adb-d945-0439-6e466c6a13cc

--- scintilla/lexers/LexSQL.cxx
+++ scintilla/lexers/LexSQL.cxx
@@ -555,7 +555,9 @@ void SCI_METHOD LexerSQL::Fold(unsigned int startPos, int length, int initStyle,
 		*/ /* notepad2-mod custom code end */
 		if (style == SCE_SQL_OPERATOR) {
 			if (ch == '(') {
-				levelNext++;
+				if (levelCurrent > levelNext)
+					levelCurrent--;
+ 				levelNext++;
 			} else if (ch == ')') {
 				levelNext--;
 			} else if ((!options.foldOnlyBegin) && ch == ';') {
