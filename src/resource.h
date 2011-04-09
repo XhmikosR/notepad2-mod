db50e75597abca430d2670a3953e3ad359f4adc3
XhmikosR <xhmikosr@users.sourceforge.net>
2011-04-09 19:14:35 +0000

    Notepad2 should compile with VS2010 Express Edition too now
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@464 28bd50df-7adb-d945-0439-6e466c6a13cc

--- src/resource.h
+++ src/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by Notepad2.rc
 //
+#define IDC_STATIC                      -1
 #define IDR_RT_MANIFEST                 1
 #define IDR_MAINWND                     100
 #define IDC_FINDTEXT                    100
