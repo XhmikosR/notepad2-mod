17e79be708dceb8acc500be9fb0c9c4f31018daa
XhmikosR <xhmikosr@users.sourceforge.net>
2011-06-10 10:34:12 +0000

    add /LARGEADDRESSAWARE, it's now possible to set a custom path to SubWCRev
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@537 28bd50df-7adb-d945-0439-6e466c6a13cc

--- update_version.bat
+++ update_version.bat
@@ -1,18 +1,21 @@
 @ECHO OFF
-SubWCRev . "src\Version_in.h" "src\Version_rev.h" -f
+SET "SUBWCREV=SubWCRev.exe"
+
+"%SUBWCREV%" . "src\Version_in.h" "src\Version_rev.h" -f
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
-SubWCRev . "res\Notepad2.exe.manifest.conf" "res\Notepad2.exe.manifest" -f >NUL
+"%SUBWCREV%" . "res\Notepad2.exe.manifest.conf" "res\Notepad2.exe.manifest" -f >NUL
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
-SubWCRev . "distrib\res\full\setup.manifest.conf" "distrib\res\full\setup.manifest" -f >NUL
+"%SUBWCREV%" . "distrib\res\full\setup.manifest.conf" "distrib\res\full\setup.manifest" -f >NUL
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
-SubWCRev . "distrib\res\lite\setup.manifest.conf" "distrib\res\lite\setup.manifest" -f >NUL
+"%SUBWCREV%" . "distrib\res\lite\setup.manifest.conf" "distrib\res\lite\setup.manifest" -f >NUL
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
 EXIT /B
 
+
 :NoSubWCRev
 ECHO NoSubWCRev, will use VERSION_REV=0
-COPY /V /Y "src\Version_rev.h.template" "src\Version_rev.h"
-COPY /V /Y "res\Notepad2.exe.manifest.template" "res\Notepad2.exe.manifest"
+COPY /V /Y "src\Version_rev.h.template"               "src\Version_rev.h"
+COPY /V /Y "res\Notepad2.exe.manifest.template"       "res\Notepad2.exe.manifest"
 COPY /V /Y "distrib\res\full\setup.manifest.template" "distrib\res\full\setup.manifest"
 COPY /V /Y "distrib\res\lite\setup.manifest.template" "distrib\res\lite\setup.manifest"
 ECHO.
