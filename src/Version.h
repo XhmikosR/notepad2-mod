50e87d02aaafe4666dd47c3970bc4cf2cf77a19c
XhmikosR <xhmikosr@users.sourceforge.net>
2012-01-02 10:39:55 +0000

    update scintilla and copyright year
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@689 28bd50df-7adb-d945-0439-6e466c6a13cc

--- src/Version.h
+++ src/Version.h
@@ -6,7 +6,7 @@
 *
 * See License.txt for details about distribution and modification.
 *
-*                                       (c) XhmikosR 2010-2011
+*                                       (c) XhmikosR 2010-2012
 *                                       http://code.google.com/p/notepad2-mod/
 *
 *
@@ -28,8 +28,8 @@
 #define MY_APPNAME                   L"Notepad2-mod"
 #define VERSION_FILEVERSION_NUM      VERSION_MAJOR,VERSION_MINOR,VERSION_BUILD,VERSION_REV
 #define VERSION_FILEVERSION          STRINGIFY(VERSION_MAJOR) ", " STRINGIFY(VERSION_MINOR) ", " STRINGIFY(VERSION_BUILD) ", " STRINGIFY(VERSION_REV)
-#define VERSION_LEGALCOPYRIGHT_SHORT L"Copyright © 2004-2011"
-#define VERSION_LEGALCOPYRIGHT_LONG  L"© Florian Balmer 2004-2011"
+#define VERSION_LEGALCOPYRIGHT_SHORT L"Copyright © 2004-2012"
+#define VERSION_LEGALCOPYRIGHT_LONG  L"© Florian Balmer 2004-2012"
 #define VERSION_AUTHORNAME           L"Florian Balmer"
 #define VERSION_WEBPAGEDISPLAY       L"flo's freeware - http://www.flos-freeware.ch"
 #define VERSION_EMAILDISPLAY         L"florian.balmer@gmail.com"
