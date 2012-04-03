6afcd5b20838a565b19c6e17665041bd7276c4c5
XhmikosR <xhmikosr@users.sourceforge.net>
2012-04-03 09:07:59 +0000

    improve r665: fix bookmarks highlighting when enabling/disabling selection margin
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@726 28bd50df-7adb-d945-0439-6e466c6a13cc

--- src/Notepad2.c
+++ src/Notepad2.c
@@ -3953,6 +3953,7 @@ LRESULT MsgCommand(HWND hwnd,WPARAM wParam,LPARAM lParam)
         else
         {
             SendMessage( hwndEdit , SCI_MARKERSETBACK , 0 , 0xff << 8 );
+            SendMessage( hwndEdit , SCI_MARKERSETALPHA , 0 , 20);
             SendMessage( hwndEdit , SCI_MARKERDEFINE , 0 , SC_MARK_BACKGROUND );
         }
 #endif
