fbb64088f62ef164d662c981990dedcb0a3599d2
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-12 11:03:51 +0000

    improve update_rev.bat
    git-svn-id: https://notepad2-mod.googlecode.com/svn/trunk@739 28bd50df-7adb-d945-0439-6e466c6a13cc

--- update_rev.bat
+++ update_rev.bat
@@ -3,25 +3,32 @@ SETLOCAL
 
 PUSHD %~dp0
 
-SET "SUBWCREV=SubWCRev.exe"
+IF EXIST "SubWCRev.exe" SET "SUBWCREV=SubWCRev.exe"
+FOR %%A IN (SubWCRev.exe) DO (SET SUBWCREV=%%~$PATH:A)
+IF NOT DEFINED SUBWCREV GOTO SubNoSubWCRev
 
 "%SUBWCREV%" . "src\Version.h.in" "src\VersionRev.h" -f
-IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
+IF %ERRORLEVEL% NEQ 0 GOTO SubError
 
 "%SUBWCREV%" . "res\Notepad2.exe.manifest.conf" "res\Notepad2.exe.manifest" -f >NUL
-IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
-
-GOTO END
-
-:NoSubWCRev
-ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
-ECHO You should (re)install TortoiseSVN.
-ECHO I'll use VERSION_REV=0 for now.
-
-TYPE "res\Notepad2.exe.manifest.template" > "res\Notepad2.exe.manifest"
-TYPE "src\VersionRev.h.template"          > "src\VersionRev.h"
-
+IF %ERRORLEVEL% NEQ 0 GOTO SubError
 :END
 POPD
 ENDLOCAL
 EXIT /B
+
+
+:SubNoSubWCRev
+ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
+ECHO You should (re)install TortoiseSVN.
+GOTO SubCommon
+
+:SubError
+ECHO Something went wrong when generating the revision number.
+
+:SubCommon
+ECHO I'll use VERSION_REV=0 for now.
+
+TYPE "res\Notepad2.exe.manifest.template" > "res\Notepad2.exe.manifest"
+TYPE "src\VersionRev.h.template"          > "src\VersionRev.h"
+GOTO END
