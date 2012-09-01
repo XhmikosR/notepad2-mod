5b20abfe8155b111c33c19b98d5a834457ef76b7
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-01 23:54:31 +0300

    switch to css-condense for minifying the css files

--- stylesheets/pack.bat
+++ stylesheets/pack.bat
@@ -5,8 +5,10 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css files...
 
-cat print.css pygment_trac.css stylesheet.css | cleancss -o pack.css
-cleancss -o print.min.css print.css && cleancss -o pygment_trac.min.css pygment_trac.css && cleancss -o stylesheet.min.css stylesheet.css
+cat print.css pygment_trac.css stylesheet.css | cssc > pack.css
+rem cssc > print.min.css print.css && ^
+rem cssc > pygment_trac.min.css pygment_trac.css && ^
+rem cssc > stylesheet.min.css stylesheet.css
 
 ENDLOCAL
 PAUSE
