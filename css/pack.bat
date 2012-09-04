2b91be0c02c3e7f99017ce0ea24b6559ba518e90
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-04 21:05:12 +0300

    update page

--- css/pack.bat
+++ css/pack.bat
@@ -5,10 +5,8 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css files...
 
-cat print.css pygment_trac.css stylesheet.css | cssc > pack.css
-rem cssc > print.min.css print.css && ^
-rem cssc > pygment_trac.min.css pygment_trac.css && ^
-rem cssc > stylesheet.min.css stylesheet.css
+cat pygment_trac.css stylesheet.css | cssc > pack.css
+cssc print.css > print.min.css
 
 ENDLOCAL
 PAUSE
