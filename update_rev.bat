a7c94b19759e69a456a7229e020d2aa6099e6956
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-15 17:26:41 +0300

    improve update_rev.bat

--- update_rev.bat
+++ update_rev.bat
@@ -3,9 +3,30 @@ SETLOCAL
 
 PUSHD %~dp0
 
+IF EXIST "build.user.bat" (CALL "build.user.bat")
+
+SET PATH=%MSYS%\bin;%PATH%
+
+FOR %%G IN (sh.exe) DO (SET FOUND=%%~$PATH:G)
+IF NOT DEFINED FOUND GOTO MissingVar
+
 sh.exe ./version.sh
+
 
 :END
 POPD
+ENDLOCAL
+EXIT /B
+
+
+:MissingVar
+COLOR 0C
+TITLE ERROR
+ECHO MSYS (sh.exe) wasn't found. Create a file build.user.bat and set the variable there.
+ECHO.
+ECHO SET "MSYS=H:\progs\MSYS"
+ECHO. & ECHO.
+ECHO Press any key to exit...
+PAUSE >NUL
 ENDLOCAL
 EXIT /B
