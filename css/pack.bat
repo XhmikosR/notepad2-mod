457e3408f066ac7517068d7f8b3db1d2564efbd1
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-26 00:01:14 +0300

    update pack.bat

--- css/pack.bat
+++ css/pack.bat
@@ -5,7 +5,7 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css files...
 
-cleancss stylesheet.css -o stylesheet.min.css
+cleancss stylesheet.css -o stylesheet.min.css && ^
 cleancss print.css -o print.min.css
 
 ENDLOCAL
