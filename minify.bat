b1533077a91aec7bb754ee0d2b0ec92380e58405
XhmikosR <xhmikosr@users.sourceforge.net>
2012-10-03 17:35:44 +0300

    add jquery.fancybox-thumbs

--- minify.bat
+++ minify.bat
@@ -5,11 +5,10 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css and js files...
 
-rem cat css\jquery.fancybox.css css\stylesheet.css | cleancss -o css\pack.css
-cleancss css\jquery.fancybox.css -o css\jquery.fancybox.min.css && ^
+cat css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss -o css\jquery.fancybox.min.css
+cat js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js && ^
 cleancss css\stylesheet.css -o css\stylesheet.min.css && ^
-cleancss css\print.css -o css\print.min.css && ^
-cat js\jquery.mousewheel.js js\jquery.fancybox.js | uglifyjs --no-copyright -o js\pack.js
+cleancss css\print.css -o css\print.min.css
 
 ENDLOCAL
 PAUSE
