b9d0329b76b099f1a137179a44c125d2f50f3335
XhmikosR <xhmikosr@users.sourceforge.net>
2012-10-15 16:28:38 +0300

    use the optimized color value and update clean-css

--- minify.bat
+++ minify.bat
@@ -5,7 +5,7 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css and js files...
 
-cat css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss -o css\jquery.fancybox.min.css
+cat css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
 cat js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js && ^
 cleancss css\stylesheet.css -o css\stylesheet.min.css && ^
 cleancss css\print.css -o css\print.min.css
