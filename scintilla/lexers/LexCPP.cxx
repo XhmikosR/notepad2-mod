639d4abc8f297579ecc7da67a70e0dd8614744c9
XhmikosR <xhmikosr@users.sourceforge.net>
2012-10-20 18:27:15 +0300

    update scintilla to 3.2.3 (9407ad7f4443 HG)

--- scintilla/lexers/LexCPP.cxx
+++ scintilla/lexers/LexCPP.cxx
@@ -579,7 +579,9 @@ void SCI_METHOD LexerCPP::Lex(unsigned int startPos, int length, int initStyle, 
 				break;
 			case SCE_C_NUMBER:
 				// We accept almost anything because of hex. and number suffixes
-				if (!(setWord.Contains(sc.ch) || ((sc.ch == '+' || sc.ch == '-') && (sc.chPrev == 'e' || sc.chPrev == 'E')))) {
+				if (!(setWord.Contains(sc.ch)
+				   || ((sc.ch == '+' || sc.ch == '-') && (sc.chPrev == 'e' || sc.chPrev == 'E' ||
+				                                          sc.chPrev == 'p' || sc.chPrev == 'P')))) {
 					sc.SetState(SCE_C_DEFAULT|activitySet);
 				}
 				break;
