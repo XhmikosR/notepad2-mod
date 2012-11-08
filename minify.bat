c36c7b2e38c5cc23ea465fb8a52d0dff80c50eb6
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-08 02:14:08 +0200

    update minify.bat

--- minify.bat
+++ minify.bat
@@ -3,8 +3,8 @@ SETLOCAL
 
 ECHO Minifying and combining css and js files...
 
-type css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
-type js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js && ^
+TYPE css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
+TYPE js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js
 cleancss css\stylesheet.css -o css\stylesheet.min.css && ^
 cleancss css\print.css -o css\print.min.css
 
