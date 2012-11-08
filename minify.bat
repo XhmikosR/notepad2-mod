2b3c7a8aa08833f5689b6d50cae81e1bb554d52f
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-08 03:30:03 +0200

    add normalize.css trimmed down to our needs

--- minify.bat
+++ minify.bat
@@ -5,8 +5,8 @@ ECHO Minifying and combining css and js files...
 
 TYPE css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
 TYPE js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js
-cleancss css\stylesheet.css -o css\stylesheet.min.css && ^
-cleancss css\print.css -o css\print.min.css
+TYPE css\stylesheet.css css\normalize.css | cleancss --s0 -o css\stylesheet.min.css
+TYPE css\print.css css\normalize.css | cleancss --s0 -o css\print.min.css
 
 ENDLOCAL
 PAUSE
