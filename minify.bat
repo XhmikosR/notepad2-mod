0aea4b8d7701b620862df2f2b15f318169488b33
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-14 12:53:22 +0200

    combine print.css to pack.css

--- minify.bat
+++ minify.bat
@@ -5,8 +5,7 @@ ECHO Minifying and combining css and js files...
 
 TYPE css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
 TYPE js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js | uglifyjs --no-copyright -o js\pack.js
-TYPE css\stylesheet.css css\normalize.css | cleancss --s0 -o css\stylesheet.min.css
-TYPE css\print.css css\normalize.css | cleancss --s0 -o css\print.min.css
+TYPE css\stylesheet.css css\print.css css\normalize.css | cleancss --s0 -o css\pack.css
 
 ENDLOCAL
 PAUSE
