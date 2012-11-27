a38c3798e2a6d8ac6ab41a8c387ecedf791117b5
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-27 19:28:45 +0200

    merge print.css to stylesheet.css

--- minify.bat
+++ minify.bat
@@ -4,7 +4,7 @@ SETLOCAL
 ECHO Minifying and combining css and js files...
 
 TYPE css\jquery.fancybox.css css\jquery.fancybox-thumbs.css | cleancss --s0 -o css\jquery.fancybox.min.css
-TYPE css\stylesheet.css css\print.css css\normalize.css | cleancss --s0 -o css\pack.css
+TYPE css\stylesheet.css css\normalize.css | cleancss --s0 -o css\pack.css
 cmd /c uglifyjs js\jquery.mousewheel.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js --compress --mangle -o js\pack.js
 
 ENDLOCAL
