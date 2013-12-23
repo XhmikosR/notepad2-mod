49cd14246450c57fd820d7bb958fc7a55fe74a6b
XhmikosR <xhmikosr@users.sourceforge.net>
2013-12-23 11:57:53 +0200

    sign.bat: re-license it under the same license Notepad2-mod is under.

--- build/sign.bat
+++ build/sign.bat
@@ -1,20 +1,18 @@
 @ECHO OFF
-REM (C) 2013 see Authors.txt
-REM
-REM This file is part of MPC-HC.
-REM
-REM MPC-HC is free software; you can redistribute it and/or modify
-REM it under the terms of the GNU General Public License as published by
-REM the Free Software Foundation; either version 3 of the License, or
-REM (at your option) any later version.
-REM
-REM MPC-HC is distributed in the hope that it will be useful,
-REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM GNU General Public License for more details.
-REM
-REM You should have received a copy of the GNU General Public License
-REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+rem ******************************************************************************
+rem *
+rem * Notepad2-mod
+rem *
+rem * sign.bat
+rem *   Batch file used to sign the binaries
+rem *   Originally taken and adapted from  https://github.com/mpc-hc/mpc-hc
+rem *
+rem * See License.txt for details about distribution and modification.
+rem *
+rem *                                       (c) XhmikosR 2013
+rem *                                       https://github.com/XhmikosR/notepad2-mod
+rem *
+rem ******************************************************************************
 
 
 SETLOCAL
