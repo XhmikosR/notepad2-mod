757797ed3075bb50b298717f05d37828ae420766
Phanx <phanx@phanx.net>
2016-08-15 19:46:14 -0700

    Add JavaScript keyword "let"

--- src/Styles.c
+++ src/Styles.c
@@ -614,7 +614,7 @@ EDITLEXER lexVB = { SCLEX_VB, 63009, L"Visual Basic", L"vb; bas; frm; cls; ctl; 
 KEYWORDLIST KeyWords_JS = {
 "abstract boolean break byte case catch char class const continue debugger default delete do "
 "double else enum export extends false final finally float for function goto if implements "
-"import in instanceof int interface long native new null package private protected public "
+"import in instanceof int interface let long native new null package private protected public "
 "return short static super switch synchronized this throw throws transient true try typeof var "
 "void volatile while with",
 "", "", "", "", "", "", "", "" };
